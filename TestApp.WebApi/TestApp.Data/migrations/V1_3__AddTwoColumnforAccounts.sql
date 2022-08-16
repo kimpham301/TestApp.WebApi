7d08fbd0ea18facb5a2304ca15bffc1d4fc33b45
Kim Pham <Kim_Pham@shi.com>
2022-08-15 20:05:27 -0500

    new migrations

--- TestApp.WebApi/TestApp.Data/migrations/V1_3__AddTwoColumnforAccounts.sql
+++ TestApp.WebApi/TestApp.Data/migrations/V1_3__AddTwoColumnforAccounts.sql
@@ -1,6 +1,8 @@
-﻿alter table Accounts
-    add column roles int not null,
-    add column tokens varchar(500) not null
+﻿alter table accounts
+    add column roles int not null;
+
+alter table accounts
+    add column tokens varchar(500) not null;
     
 
     
