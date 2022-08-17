ae33e28aa5d850e1b3042529d9763e7ee43e9f72
Kim Pham <Kim_Pham@shi.com>
2022-08-17 09:54:40 -0500

    hashpass

--- TestApp.WebApi/Authorization/hashPassword.cs
+++ TestApp.WebApi/Authorization/hashPassword.cs
@@ -11,7 +11,7 @@ namespace TestApp.WebApi.Authorization
             var sha = SHA256.Create();
             var asByteArray = Encoding.Default.GetBytes(password);
             var hashedPassword = sha.ComputeHash(asByteArray);
-            return Convert.ToBase64String(asByteArray);
+            return Convert.ToBase64String(hashedPassword);
         }
     }
 }
